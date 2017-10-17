bb5ccfe326c1e85a7f251f29dcbdd8095c6a1bc6
1027 <ckdghwh@nate.com>
2017-10-17 10:22:50 +0900

    Initial commit

---
+++ basicServer.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(3303, function () {
+    console.log('Server Start');
+});
